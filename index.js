ab94e8640dc96e656138c4dbe0cbe1630459fedc
ShreyanDey0 <shreyandey5@gmail.com>
2021-10-12 12:44:14 +0530

    static but not responsive

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#sidebarbtn").click(function () {
+        $(".sidebar").show();
+    });
+    $(".sidebar").click(function () {
+        $(".sidebar").hide();
+    });
+});
